5ecc8dcdf404cfaf79a0dd5eb2a37c4a2f5e250b
jmacc <j2macc@gmail.com>
2023-01-03 17:56:56 -0600

    variables-

--- Variables.py
+++ Variables.py
@@ -1,3 +1,9 @@
+print(1,2,3,4,5)
+print(1,2,3,4,5, sep=",", end=".")
+
+my_string_variable ="La primera varaible"
+print(my_string_variable)
+
 my_init_variable = 5
 print(my_init_variable)
 
@@ -7,3 +13,21 @@ print(type(my_init_to_str_variable)) #indica el tipo de dato
 
 my_bool_variable = False
 print(my_bool_variable)
+
+#concatenacion de variables de un print
+print(my_init_variable,my_init_to_str_variable,my_bool_variable)
+
+#funciones del sistema
+print(len(my_string_variable))
+
+#variables en una sola linea
+name, subname, alias, edad  = "Jorge","Castillo","Perico",35
+print("Mi llamo",name,"Mi apellido es",subname,"Tengo la edad de ",edad,"años y me dicen",alias)
+
+#imputs
+
+name = input ("Hola, como te llamas?")
+age = input("¡Cuantos años tienes?")
+
+print (name)
+print (age)
