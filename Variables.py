7f3591455c286a9c19e2021d2a7b11c329edce6a
jmacc <j2macc@gmail.com>
2023-01-03 14:01:26 -0600

    variables

---
+++ Variables.py
@@ -0,0 +1,9 @@
+my_init_variable = 5
+print(my_init_variable)
+
+my_init_to_str_variable =str(my_init_variable)
+print(my_init_to_str_variable)
+print(type(my_init_to_str_variable)) #indica el tipo de dato
+
+my_bool_variable = False
+print(my_bool_variable)
