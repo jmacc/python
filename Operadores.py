70bb52afa1d19fa87dd77c4d8c565b7a080d91c1
jmacc <j2macc@gmail.com>
2023-01-03 18:23:44 -0600

    Operadores

---
+++ Operadores.py
@@ -0,0 +1,25 @@
+#Operadores
+
+Sumando1 = float(input("Ingresa el primer valor decimal: "))
+Sumando2 = float(input("Ingresa el segundo valor decimal: "))
+
+print("El resultado de la suma es :",Sumando1+Sumando2)
+
+print(3+4)
+print(10 % 2)
+print( 10 // 3)
+print(2 ** 3)
+print(2 * 3)
+print("hola" + str(5))
+
+print(3 == 5)
+print(2 < 5)
+print(6 > 8)
+print(7 != 8)
+
+print("abcd" > "e") #10rden alfabetico
+
+#OperadoresLogicos
+
+print(5 > 5 and 3 > 6)
+print(5 > 5 or 3 > 6)
