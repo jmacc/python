f0f57f0953e42a01054a8a7d2207ebfa5b8a21a5
jmacc <j2macc@gmail.com>
2023-01-13 23:36:01 -0600

    anexos

---
+++ Bucles.py
@@ -0,0 +1,32 @@
+#Bucles
+
+#While
+"""
+dia = 0    
+semana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo','Terminator']
+while dia < 4:
+   print("Hoy es " + semana[dia])
+   dia += 1
+"""
+#-------------------------------------------------
+"""
+num = 0
+while num < 10:
+    print(num,end=" ")
+    num += 1
+"""
+#-------------------------------------------------
+"""
+datos = True
+while datos:
+    valor = int(input("Ingresa un valor mayor a 10 "))
+    if valor > 10:
+        datos = False
+    print("Programa Finalizado")
+"""
+#------------------------------------------------------    
+#For
+lista =[1,2,3,4,"casa","coche","avion"]
+for item in lista:
+    print(lista)
+
