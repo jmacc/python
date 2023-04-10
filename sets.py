272485e41211e4836503f5dd22a1ff882c9f1547
jmacc <j2macc@gmail.com>
2023-04-09 21:43:52 -0600

    ejemplos

---
+++ sets.py
@@ -0,0 +1,11 @@
+#sets
+
+my_set = set()
+my_other_set ={"Mario",12,345,7689,12,345,67,90,"Jose"}
+
+print(type(my_set))
+print(type(my_other_set))
+print(len(my_other_set))
+
+my_other_set.add("jmacc")
+print(my_other_set)
---
+++ type_hints.py
@@ -0,0 +1,9 @@
+#type_hints
+
+my_string_variable ="My string variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_type_variable:str = "my  typed string variable"
+print(my_type_variable)
+print(type(my_type_variable))
