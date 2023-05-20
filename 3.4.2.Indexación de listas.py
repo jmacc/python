0eac5f228636c8b501cbb87393e4955dbe6e17d2
jmacc <j2macc@gmail.com>
2023-05-20 17:17:08 -0600

    3.4.9.Haciendo uso de las listas

--- 3.4.2.Indexación de listas.py
+++ 3.4.2.Indexación de listas.py
@@ -26,7 +26,7 @@ print(numbers)
 print(numbers[-3]) #a partir del ultimo de la lista
 
 ##############################################
-"""
+
 dato = 0
 rabbit=[1,2,3,4,5]
 while dato <= 5:
@@ -35,4 +35,56 @@ while dato <= 5:
     rabbit.remove(number) #eliminar el valor ingresado
     rabbit.append(new_number) #agrega el nuevo valor a la lista
     print(rabbit) #imprime la nueva lista
+##############################################
 
+numbers = [111, 7, 2, 1]
+print(len(numbers))
+print(numbers)
+
+###
+
+numbers.append(4)
+
+print(len(numbers))
+print(numbers)
+
+###
+
+numbers.insert(0, 222)
+print(len(numbers))
+print(numbers)
+
+##############################################
+
+my_list = []  # Creando una lista vacía.
+
+for i in range(5):
+    my_list.append(i + 1) #agrega un elemento a la ultima posicion
+
+print(my_list)
+##############################################
+
+my_list = []  # Creando una lista vacía.
+ 
+for i in range(5):
+    my_list.insert(0, i + 1) #agregar el numero a la primera posicion
+ 
+print(my_list)
+##############################################
+
+my_list = [10, 1, 8, 3, 5]
+total = 0
+
+for i in range(len(my_list)):
+    total += my_list[i]
+
+print(total)
+##############################################
+"""
+my_list = [10, 1, 8, 3, 5]
+total = 0
+
+for i in my_list:
+    total += i
+ 
+print(total)
--- Recopilacion de Entradas.py
+++ Recopilacion de Entradas.py
@@ -5,3 +5,17 @@ print(sys.argv)
 print(sys.argv[0]) # program name
 print("1"+2)
 #print(sys.argv[1]) # first arg
+'''
+Limpieza de paquetes no utilizados
+A veces, es posible que se dé cuenta de que ya no necesita un determinado paquete de Python
+y, por tanto, desea quitarlo. En ese caso, puede usar : pip uninstall python-dateutil
+'''
+
+'''
+Para borrar todo lo que dependía de este paquete, puede escribir 
+todos los paquetes instalados en una lista derequirements.txt, de la siguiente manera:
+pip freeze > requirements.txt
+
+A continuación, quite todos los paquetes de esa lista, de la siguiente manera:
+pip uninstall -r requirements.txt -y
+'''
