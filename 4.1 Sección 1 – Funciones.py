954b2610c2396a7097e77175434ad5d2427c654c
jmacc <j2macc@gmail.com>
2023-06-12 17:29:16 -0600

    Funciones 4.1

---
+++ 4.1 Sección 1 – Funciones.py
@@ -0,0 +1,14 @@
+#4.1 Sección 1 - Funciones
+'''
+##############################################
+'''
+def message():
+    print("Ingresar valor: ")
+ 
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
+ 
